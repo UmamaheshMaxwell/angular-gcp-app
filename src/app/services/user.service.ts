712c1a0770ae141196f770a2cf7cd25a909dba74
Umamaheswararao Meka <maheshmeka16@gmail.com>
2023-08-07 14:03:45 +0000

    added node service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { 
+  apiUrl = 'https://node-gcp-service-3imv474m7a-uc.a.run.app';
 
+  constructor(private http: HttpClient) { }
+
+  health(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/`);
+  }
+  
+  getUsers(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/users`);
   }
 
-  
+
 }
