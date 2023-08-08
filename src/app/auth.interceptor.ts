f9b6718e32e367223543fa461419865bc4114838
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-08-08 12:00:17 +0530

    added interceptor to authenticate the request

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,28 +1,53 @@
-import { Injectable, Injector } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable, from } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { AuthService } from './services/auth/auth.service';
+import { catchError, switchMap, finalize } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
   constructor(private authService: AuthService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // Call getIdentityToken from AuthService
+    return this.getTokenAndSetHeaders(request, next);
+  }
+
+  private getTokenAndSetHeaders(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return this.authService.getIdentityToken().pipe(
       switchMap((token: string) => {
-        // Add the token to the "Authorization" header of the request
-        console.log(`token : ${token}`)
-        const authRequest = request.clone({
-          setHeaders: { 
-            Authorization: `Bearer ${token}`
-          }
-        });
-        // Pass the modified request to the next interceptor or to the HTTP handler
-        return next.handle(authRequest);
+        console.log(`Identity token fetched: ${token}`);
+        const authRequest = this.cloneRequestWithAuthorizationHeader(request, token);
+        return this.handleRequestWithLogging(authRequest, next);
+      }),
+      catchError(error => {
+        console.error('Error fetching identity token:', error);
+        return next.handle(request); // Continue with the original request
+      })
+    );
+  }
+
+  private cloneRequestWithAuthorizationHeader(request: HttpRequest<any>, token: string): HttpRequest<any> {
+    return request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+
+  private handleRequestWithLogging(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('Sending request with authorization header:', request);
+    const startTime = Date.now();
+    return next.handle(request).pipe(
+      finalize(() => {
+        const duration = Date.now() - startTime;
+        console.log(`Response received in ${duration}ms`);
       })
     );
   }
 }
+
+
+
+
+
+
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -5,26 +5,15 @@ import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
-    url = 'https://node-gcp-service-3imv474m7a-uc.a.run.app/';
+    backendUrl = 'https://node-gcp-service-3imv474m7a-uc.a.run.app';
     audience = 'https://angular-gcp-service-3imv474m7a-uc.a.run.app';
 
   constructor(private http: HttpClient) {}
 
-  // getIdentityToken(): Observable<string> {
-  //   const headers = new HttpHeaders({'Metadata-Flavor': 'Google'});
-  //   const url = `http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=${this.audience}`;
-  //   return this.http.get(url, { headers, responseType: 'text' });
-  // }
   getIdentityToken(): Observable<string> {
-    const headers = new HttpHeaders({ 'Metadata-Flavor': 'Google' });
+    const headers = new HttpHeaders({'Metadata-Flavor': 'Google'});
     const url = `http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=${this.audience}`;
-
-    return this.http.get(url, { headers, responseType: 'text' }).pipe(
-      catchError(error => {
-        console.error('Error fetching identity token:', error);
-        return throwError('Failed to fetch identity token');
-      })
-    );
+    return this.http.get(url, { headers, responseType: 'text' });
   }
 }
 
