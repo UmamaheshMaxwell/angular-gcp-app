f19130f024289f417d9a2ea6e4b9107bc284ed06
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-08-08 11:07:38 +0530

    added interceptor to authenticate the request

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -10,10 +10,21 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
+  // getIdentityToken(): Observable<string> {
+  //   const headers = new HttpHeaders({'Metadata-Flavor': 'Google'});
+  //   const url = `http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=${this.audience}`;
+  //   return this.http.get(url, { headers, responseType: 'text' });
+  // }
   getIdentityToken(): Observable<string> {
-    const headers = new HttpHeaders({'Metadata-Flavor': 'Google'});
+    const headers = new HttpHeaders({ 'Metadata-Flavor': 'Google' });
     const url = `http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=${this.audience}`;
-    return this.http.get(url, { headers, responseType: 'text' });
+
+    return this.http.get(url, { headers, responseType: 'text' }).pipe(
+      catchError(error => {
+        console.error('Error fetching identity token:', error);
+        return throwError('Failed to fetch identity token');
+      })
+    );
   }
 }
 
